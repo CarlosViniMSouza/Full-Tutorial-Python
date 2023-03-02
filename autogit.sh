0699d9a5b60ed3d4aefae5cd9894175b71a26193
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2023-03-02 00:12:06 -0300

    World 1 -> user input

--- autogit.sh
+++ autogit.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -m "World 1 -> type cast"
+git commit -m "World 1 -> user input"
 git push -u origin main
 git status
 
-# stop moment -> 25:20
+# stop moment -> 30:16
--- src/world01/script.py
+++ src/world01/script.py
@@ -1,37 +1,61 @@
 ## START of multiple assignment
 
-# way 1:
-name = "Carlos"
-age = 22
-status = True
+class MultipleAssignment:
+    # way 1:
+    name = "Carlos"
+    age = 22
+    status = True
 
-print(f"\nHy, I'm {name}, my age is {age}, and my student status is {status}")
-# output: Hy, I'm Carlos, my age is 22, and my student status is True
+    print(f"\nHy, I'm {name}, my age is {age}, and my student status is {status}")
+    # output: Hy, I'm Carlos, my age is 22, and my student status is True
 
-# way 2:
-name, age, status = "Carlos Souza", 20, False
+    # way 2:
+    name, age, status = "Carlos Souza", 20, False
 
-print(f"Hy, I'm {name}, my age is {age}, and my student status is {status}")
-# output: Hy, I'm Carlos Souza, my age is 20, and my student status is False
+    print(f"Hy, I'm {name}, my age is {age}, and my student status is {status}")
+    # output: Hy, I'm Carlos Souza, my age is 20, and my student status is False
 
 ## END of multiple assignment
 
 ## START of String Methods
 
-name = "CarlosViniMSouza"
+class StringMethods:
+    name = "CarlosViniMSouza"
 
-print(f"\nLength of variable = {len(name)}") 
-# output: 16
+    print(f"\nLength of variable = {len(name)}") 
+    # output: 16
 
-print(f"Find one char = {name.find('C')}") # 0 -> found | -1 -> Not found
+    print(f"Find one char = {name.find('C')}") # 0 -> found | -1 -> Not found
 
-print(f"Capitalized = {name.capitalize()}")
-# output: Carlosvinimsouza
+    print(f"Capitalized = {name.capitalize()}")
+    # output: Carlosvinimsouza
 
-print(f"Is digit? = {name.isdigit()}")
-# output: False
+    print(f"Is digit? = {name.isdigit()}")
+    # output: False
 
-print(f"Replacing M by Z = {name.replace('M', 'Z')}")
-# output: CarlosViniZSouza
+    print(f"Replacing M by Z = {name.replace('M', 'Z')}")
+    # output: CarlosViniZSouza
 
 ## END of String Methods
+
+## START of Type Cast
+
+# type casting -> convert the data for other type data!
+
+class TypeCast:
+    type01, type02 = 10, 8.5
+    type03, type04 = "My Name", 'C'
+    type05, type06 = True, "19"
+
+    print(f"\nType of var type04 = {type(type05)}")
+    # output: Type of var type04 = <class 'bool'>
+
+    print(f"Converting type01 for string = {type(str(type01))}")
+    # output: Converting type01 for string = <class 'str'>
+
+    print(f"Converting type06 for integer = {type(int(type06))}")
+    # output: Converting type01 for string = <class 'int'>
+
+## END of Type Cast
+
+## START of User Input
